f9620c0a236ee1195bf461d0afd1e37ae58ba3e1
Glauber de Oliveira Matos <glauber.matos@IFBAIANO.SI.INTRANET>
2021-11-18 17:12:00 -0300

    api routes and authentication with Next Auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,27 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+ 
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             type="button"
             className={styles.singInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04D361" />
-            glaubermatos
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type="button"
             className={styles.singInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color="#eba417" />
             Sing in with GitHub
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
 
-import styles from '../styles/pages/home.module.scss'
+import styles from './home.module.scss'
 
 interface HomeProps {
   product: {
