9dff1a51174a4b6869bd67ec68dd83fd2a572807
glaubermatos <glaub.oliveira@hotmail.com>
2021-12-06 09:32:50 -0300

    stripe payments, checkout, redirect and avoiding duplicate customers

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client'
+import getStripeJS from '../../services/stripe-js'
+import { api } from '../../services/api'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +8,37 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+    //tem acesso a sessão do usuário
+    const [session] = useSession()
+    
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        try {
+            //se tem um usuário logado na aplicação
+            //criar um checkout sessionID usando a api-route do nextjs
+            const response = await api.post('/subscribe')
+            
+            const { sessionId } = response.data
+            
+            const stripeJS = await getStripeJS()
+            
+            //redirect para o checkout session do stripe usando o sdk 
+            //para uso do lado front-end, stripe-js para cliente js
+            await stripeJS.redirectToCheckout({sessionId})
+        } catch (err) {
+            alert(err.message)
+        }
+    }
+
     return(
         <button 
             type="button" 
             className={styles.subscribeButton}
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
